381b07ce7035145cb9f649d4b649d2aa6a45de8a
Kazuhiro Osawa <yappo@shibuya.pl>
2012-08-09 16:46:21 +0900

    initial commit

---
+++ lib/Parse/JapaneseZipCode.pm
@@ -0,0 +1,32 @@
+package Parse::JapaneseZipCode;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Parse::JapaneseZipCode -
+
+=head1 SYNOPSIS
+
+    use Parse::JapaneseZipCode;
+
+=head1 DESCRIPTION
+
+Parse::JapaneseZipCode is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
